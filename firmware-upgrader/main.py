cfad2e3ed7b8756f4caced83e1e3c86c315883e6
lukas <lukas.auer@gmail.com>
2018-10-24 08:31:57 +0200

    added flask web app

--- firmware-upgrader/main.py
+++ firmware-upgrader/main.py
@@ -1,13 +1,26 @@
+import logging
 import re
 import urllib3
 
+from flask import Flask
+from flask import render_template
 from nornir.core import InitNornir
 from nornir.plugins.tasks import networking
 from nornir.plugins.tasks.networking import napalm_get
 from nornir.plugins.functions.text import print_result
 
+app = Flask(__name__)
+
 
 def get_firmware_version(task):
+    """
+    runs napalm get_facts, extracts firmware version and saves it in the inventory
+    as a host variable 'firmware'
+    note: depending on the OS, napalm returns the 'os version' in different formats,
+    somtimes just the version jumber (9.2(1)) and sometimes
+    :param task:
+    :return:
+    """
     # use napalm to get device facts
     r = task.run(task=networking.napalm_get, getters=['facts'])
 
@@ -27,23 +40,39 @@ def get_firmware_version(task):
     task.host['firmware'] = firmware
 
 
-def run_nornir():
-    # suppress InsecureRequestWarnings
-    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+@app.route("/")
+def start():
+    return render_template('index.html')
 
+
+@app.route("/firmware")
+def run_nornir():
     # nr = InitNornir(config_file='nornir/config.yaml')
     nr = InitNornir(config_file='nornir/config.yaml', dry_run=True)
 
     # apply filter to inventory
-    inv = nr.filter(site='cisco')
+    cisco = nr.filter(site='cisco')
 
     # run task 'get_firmware_version'
-    result = inv.run(task=get_firmware_version)
+    result = cisco.run(task=get_firmware_version)
 
     # show firmware versions
-    for h in inv.inventory.hosts.keys():
-        print('{h}: {f}'.format(h=inv.inventory.hosts[h]['name'],f=inv.inventory.hosts[h]['firmware']))
+    devices = {}
+    for h in cisco.inventory.hosts.keys():
+        devices[cisco.inventory.hosts[h]['name']] = cisco.inventory.hosts[h]['firmware']
+        # print('{h}: {f}'.format(h=cisco.inventory.hosts[h]['name'], f=cisco.inventory.hosts[h]['firmware']))
+
+    return render_template('firmware.html', devices=devices)
 
 
 if __name__ == '__main__':
-    run_nornir()
+    logging.basicConfig(filename='../log/app.log', level=logging.INFO)
+
+    logging.debug('disabling InsecureRequestWarnings...')
+    # suppress InsecureRequestWarnings
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+    # run flask web app
+    logging.info('starting web application..')
+    app.run(debug=True)
+    # run_nornir()
