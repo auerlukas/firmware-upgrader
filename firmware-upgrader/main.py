632850be4956bdfd9f866c44f2bfffc9a059cfce
lukas <lukas.auer@gmail.com>
2018-10-25 11:12:37 +0200

    added very basic vulnerability checker

--- firmware-upgrader/main.py
+++ firmware-upgrader/main.py
@@ -1,6 +1,13 @@
+import json
 import logging
 import re
+import sys
 import urllib3
+
+import openVulnQuery
+# from openVulnQuery.query_client import OpenVulnQueryClient
+# from openVulnQuery.advisory import AdvisoryIOS
+from typing import List
 
 from flask import Flask
 from flask import render_template
@@ -8,8 +15,28 @@ from nornir.core import InitNornir
 from nornir.plugins.tasks import networking
 from nornir.plugins.tasks.networking import napalm_get
 from nornir.plugins.functions.text import print_result
-
 app = Flask(__name__)
+
+
+def find_vulnerabilities(version: str) -> List[openVulnQuery.advisory.AdvisoryIOS]:
+    """
+    finds all vulnerabilities of a given Cisco switch model by querying the Cisco openvuln API
+    (only works for devices running IOS since function 'get_by_ios' is being used
+    :param version: Cisco switch model running IOS
+    :return: returns a list of all found vulnerabilities
+    """
+    try:
+        with(open('openvuln/openvuln_api_credentials.json', 'r')) as data:
+            credentials = json.load(data)
+    except FileNotFoundError as e:
+        print('file with openvuln api credentials could not be found!\n{e}'.format(e=e), file=sys.stderr)
+        sys.exit(1)
+
+    openVulnQuery.query_client.OpenVulnQueryClient
+    query_client: openVulnQuery.query_client.OpenVulnQueryClient = openVulnQuery.query_client.OpenVulnQueryClient(client_id=credentials['CLIENT_ID'],
+                                                            client_secret=credentials['CLIENT_SECRET'])
+    advisories: List[openVulnQuery.advisory.AdvisoryIOS] = query_client.get_by_ios('', version)
+    return advisories
 
 
 def get_firmware_version(task):
@@ -44,9 +71,11 @@ def get_firmware_version(task):
 def start():
     return render_template('index.html')
 
+
 @app.route("/inventory")
 def inventory():
     return render_template('inventory.html')
+
 
 @app.route("/firmware")
 def run_nornir():
@@ -67,12 +96,17 @@ def run_nornir():
 
     return render_template('firmware.html', devices=devices)
 
+
 @app.route("/vulnerabilities")
 def vulnerabilities():
-    return render_template('vulnerabilities.html')
+    version = '12.2(55)SE11'
+    vulnerabilities = find_vulnerabilities(version)
+    return render_template('vulnerabilities.html', vulnerabilities=vulnerabilities)
 
 
 if __name__ == '__main__':
+    print("PATH: ")
+    print(sys.path)
     logging.basicConfig(filename='../log/app.log', level=logging.INFO)
 
     logging.debug('disabling InsecureRequestWarnings...')
